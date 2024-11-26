5510910c36a854b58130beae960058c59aff4a01
Chirag Patil <patilchirag192k@gmail.com>
2024-11-26 15:08:17 +0530

    Added: UC2 EMployee Wage

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -2,11 +2,17 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
         final int IS_PRESENT = 1;
+        final int WAGE_PER_HOUR = 20;
+        final int FULL_DAY_HOUR = 8;
+        int workingHours = 0;
         int attendance = (int) (Math.random() * 2);
         if (attendance == IS_PRESENT) {
             System.out.println("Employee is Present");
+            workingHours = 8;
         } else {
             System.out.println("Employee is Absent");
         }
+        int dailyWage = WAGE_PER_HOUR * workingHours;
+        System.out.println("Daily Employee Wage : " + dailyWage);
     }
 }
