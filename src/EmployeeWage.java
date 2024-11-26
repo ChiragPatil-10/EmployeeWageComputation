ad1eb342ee31d1afcb1725caac5a0949dabfc865
Chirag Patil <patilchirag192k@gmail.com>
2024-11-26 21:40:03 +0530

    Added : UC5 Wages for Month

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -5,24 +5,29 @@ public class EmployeeWage {
         final int PART_TIME = 2;
         final int WAGE_PER_HOUR = 20;
         final int FULL_DAY_HOUR = 8;
-        int workingHours = 0;
-        int attendance = (int) (Math.random() * 3);
-        switch (attendance)
+        final int WORKING_DAYS = 20;
+        int totalWage = 0;
+        for (int day = 1; day <= WORKING_DAYS; day++)
         {
-            case IS_PRESENT:
-                System.out.println("Employee is Present Full Time ");
-                workingHours = 8;
-                break;
-            case PART_TIME:
-                System.out.println("Employee is Present Part Time");
-                workingHours = 4;
-                break;
-            default:
-                System.out.println("Employee is Absent");
+            int attendance = (int) (Math.random() * 3);
+            int workingHours = 0;
+            switch (attendance)
+            {
+                case IS_PRESENT:
+                    System.out.println("Employee is Present Full Time ");
+                    workingHours = 8;
+                    break;
+                case PART_TIME:
+                    System.out.println("Employee is Present Part Time");
+                    workingHours = 4;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+            }
+            int wage = WAGE_PER_HOUR * workingHours;
+            System.out.println("Day " + day + " wage is: " + wage);
+            totalWage += wage;
         }
-        int dailyWage = WAGE_PER_HOUR * workingHours;
-        System.out.println("Daily Employee Wage : " + dailyWage);
-        
-
+        System.out.println("Total Wage of Employee for a month is: " + totalWage);
     }
 }
