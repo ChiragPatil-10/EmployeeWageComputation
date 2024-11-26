b1e229fd9388bf43ea0e273941c8be40387aa687
Chirag Patil <patilchirag192k@gmail.com>
2024-11-26 14:19:22 +0530

    Added: UC1 Employee Present or Absent

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,7 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
+        final int IS_PRESENT = 1;
+        int attendance = (int) (Math.random() * 2);
+        if (attendance == IS_PRESENT) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
