bd12d47ad025a76482c2ec45db3ddc029d9d4a95
Chirag Patil <patilchirag192k@gmail.com>
2024-11-26 15:35:18 +0530

    Added: UC3 Part time Empolyee

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -2,17 +2,25 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
         final int IS_PRESENT = 1;
+        final int PART_TIME = 2;
         final int WAGE_PER_HOUR = 20;
         final int FULL_DAY_HOUR = 8;
         int workingHours = 0;
-        int attendance = (int) (Math.random() * 2);
+        int attendance = (int) (Math.random() * 3);
         if (attendance == IS_PRESENT) {
             System.out.println("Employee is Present");
             workingHours = 8;
-        } else {
+        }
+        else if (attendance == PART_TIME) {
+            System.out.println("Employee is Present Part Time");
+            workingHours = 4;
+        }
+        else {
             System.out.println("Employee is Absent");
         }
         int dailyWage = WAGE_PER_HOUR * workingHours;
         System.out.println("Daily Employee Wage : " + dailyWage);
+        
+
     }
 }
